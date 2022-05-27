6366c9a7a9dcf5030c38fa9e1da125db17e79e6a
Shujing He <heshu.jing@163.com>
2022-05-27 10:42:58 +0800

    test new

---
+++ test/main.mm
@@ -0,0 +1,44 @@
+//
+//  main.m
+//  test
+//
+//  Created by ByteDance on 2022/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myThread : NSObject
+
+@property (nonatomic, strong) NSThread * thread;
+
+@end
+
+@implementation myThread
+
+-(void) start {
+    _thread = [[NSThread alloc] initWithTarget:self selector:@selector(func) object:nil];
+    [_thread start];
+    NSLog(@"Hello, World!");
+}
+
+-(void) func {
+    int num;
+    while (1) {
+        scanf("%d", &num);
+        printf("input number = %d\n", num);
+    }
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        myThread * t = [[myThread alloc] init];
+        [t start];
+        while (1) {
+            sleep(5);
+            printf("5 second elapsed!\n");
+        }
+    }
+    return 0;
+}
